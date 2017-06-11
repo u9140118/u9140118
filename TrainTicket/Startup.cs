ca924f497a8ca8a41a23f2e6a9926f4e323011e8
vince_lai <u9140118@hotmail.com>
2017-06-11 16:41:01 +0800

    加入專案檔案。

---
+++ TrainTicket/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrainTicket.Startup))]
+namespace TrainTicket
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
